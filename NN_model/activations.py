5421913af44827620946a3791f74b58f33f44b7a
ViriAldi <v.fedynyak@ucu.edu.ua>
2020-06-08 14:37:29 +0300

    ended layer

--- NN_model/activations.py
+++ NN_model/activations.py
@@ -5,13 +5,21 @@ def sigmoid(matrix):
     return 1 / (1 + np.exp(-matrix))
 
 
+def sigmoid_derivative(matrix):
+    return np.exp(-matrix) / (1 + np.exp(-matrix))**2
+
+
 def tanh(matrix):
     return np.tanh(matrix)
+
+
+def tanh_derivative(matrix):
+    return 4 * np.exp(2 * matrix) / (1 + np.exp(2 * matrix))**2
 
 
 def relu(matrix):
     return max(matrix, 0)
 
 
-def leaky_relu(matrix):
-    return max(matrix, -0.01 * matrix)
+def relu_derivative(matrix):
+    return (matrix > 0).astype(int)
--- NN_model/layer.py
+++ NN_model/layer.py
@@ -2,18 +2,28 @@ import numpy as np
 
 
 class Layer:
-    def __init__(self, n, n_prev, num, activation):
+    def __init__(self, n, n_prev, num, activation, derivative):
         self.num = num
         self.activation = activation
+        self.derivative = derivative
         self.size = n
         self.prev = n_prev
         self.W = np.zeros((n, n_prev))
         self.B = np.zeros((n, 1))
-        self.W = np.zeros((n, n_prev))
-        self.B = np.zeros((n, 1))
+        self.dW = np.zeros((n, n_prev))
+        self.dB = np.zeros((n, 1))
         self.A = None
         self.Z = None
         self.dA = None
         self.dZ = None
 
     def forward_propagate(self, A_prev):
+        self.Z = self.W @ A_prev + self.B
+        self.A = self.activation(self.Z)
+
+    def backward_propagate(self, dA, A_prev, m):
+        self.dA = dA
+        self.dZ = self.dA * self.derivative(self.Z)
+        self.dB = (1 / m) * np.sum(self.dZ, axis=1, keepdims=True)
+        self.dW = (1 / m) * self.dZ * A_prev.T
+        return self.W.T * self.dZ
